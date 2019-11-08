5f62d4042870a8240364e4c0d4f99f5a6243e336
Christine Wright <meai222@live.rhul.ac.uk>
2019-11-08 15:08:10 +0000

    Updated sphincsplus-polyvec.h

--- sphincsplus-polyvec.h
+++ sphincsplus-polyvec.h
@@ -2,66 +2,66 @@
 #define POLYVEC_H
 
 #include <stdint.h>
-#include "dilithium-params.h"
-#include "dilithium-poly.h"
+#include "sphincsplus-params.h"
+#include "sphincsplus-poly.h"
 
 /* Vectors of polynomials of length L */
 typedef struct {
-  dilithium_poly vec[5]; // Max L
-} dilithium_polyvecl;
+  sphincsplus_poly vec[5]; // Max L
+} sphincsplus_polyvecl;
 
-void dilithium_polyvecl_freeze(dilithium_polyvecl *v, uint64_t dilithium_l);
+void sphincsplus_polyvecl_freeze(sphincsplus_polyvecl *v, uint64_t sphincsplus_l);
 
-void dilithium_polyvecl_add(dilithium_polyvecl *w, const dilithium_polyvecl *u,
-        const dilithium_polyvecl *v, uint64_t dilithium_l);
+void sphincsplus_polyvecl_add(sphincsplus_polyvecl *w, const sphincsplus_polyvecl *u,
+        const sphincsplus_polyvecl *v, uint64_t sphincsplus_l);
 
-void dilithium_polyvecl_ntt(dilithium_polyvecl *v, uint64_t dilithium_l);
-void dilithium_polyvecl_pointwise_acc_invmontgomery(dilithium_poly *w,
-                                          const dilithium_polyvecl *u,
-                                          const dilithium_polyvecl *v,
-                                          uint64_t dilithium_l);
+void sphincsplus_polyvecl_ntt(sphincsplus_polyvecl *v, uint64_t sphincsplus_l);
+void sphincsplus_polyvecl_pointwise_acc_invmontgomery(sphincsplus_poly *w,
+                                          const sphincsplus_polyvecl *u,
+                                          const sphincsplus_polyvecl *v,
+                                          uint64_t sphincsplus_l);
 
-int dilithium_polyvecl_chknorm(const dilithium_polyvecl *v, uint32_t B,
-        uint64_t dilithium_l);
+int sphincsplus_polyvecl_chknorm(const sphincsplus_polyvecl *v, uint32_t B,
+        uint64_t sphincsplus_l);
 
 
 
 /* Vectors of polynomials of length K */
 typedef struct {
-  dilithium_poly vec[6]; // Max K
-} dilithium_polyveck;
+  sphincsplus_poly vec[6]; // Max K
+} sphincsplus_polyveck;
 
-void dilithium_polyveck_reduce(dilithium_polyveck *v, uint64_t dilithium_k);
-void dilithium_polyveck_csubq(dilithium_polyveck *v, uint64_t dilithium_k);
-void dilithium_polyveck_freeze(dilithium_polyveck *v, uint64_t dilithium_k);
+void sphincsplus_polyveck_reduce(sphincsplus_polyveck *v, uint64_t sphincsplus_k);
+void sphincsplus_polyveck_csubq(sphincsplus_polyveck *v, uint64_t sphincsplus_k);
+void sphincsplus_polyveck_freeze(sphincsplus_polyveck *v, uint64_t sphincsplus_k);
 
-void dilithium_polyveck_add(dilithium_polyveck *w,
-        const dilithium_polyveck *u, const dilithium_polyveck *v,
-        uint64_t dilithium_k);
-void dilithium_polyveck_sub(dilithium_polyveck *w,
-        const dilithium_polyveck *u, const dilithium_polyveck *v,
-        uint64_t dilithium_k);
-void dilithium_polyveck_shiftl(dilithium_polyveck *v, uint64_t dilithium_k);
+void sphincsplus_polyveck_add(sphincsplus_polyveck *w,
+        const sphincsplus_polyveck *u, const sphincsplus_polyveck *v,
+        uint64_t sphincsplus_k);
+void sphincsplus_polyveck_sub(sphincsplus_polyveck *w,
+        const sphincsplus_polyveck *u, const sphincsplus_polyveck *v,
+        uint64_t sphincsplus_k);
+void sphincsplus_polyveck_shiftl(sphincsplus_polyveck *v, uint64_t sphincsplus_k);
 
-void dilithium_polyveck_ntt(dilithium_polyveck *v, uint64_t dilithium_k);
-void dilithium_polyveck_invntt_montgomery(dilithium_polyveck *v,
-        uint64_t dilithium_k);
+void sphincsplus_polyveck_ntt(sphincsplus_polyveck *v, uint64_t sphincsplus_k);
+void sphincsplus_polyveck_invntt_montgomery(sphincsplus_polyveck *v,
+        uint64_t sphincsplus_k);
 
-int dilithium_polyveck_chknorm(const dilithium_polyveck *v, uint32_t B,
-        uint64_t dilithium_k);
+int sphincsplus_polyveck_chknorm(const sphincsplus_polyveck *v, uint32_t B,
+        uint64_t sphincsplus_k);
 
-void dilithium_polyveck_power2round(dilithium_polyveck *v1,
-        dilithium_polyveck *v0, const dilithium_polyveck *v,
-        uint64_t dilithium_k);
-void dilithium_polyveck_decompose(dilithium_polyveck *v1,
-        dilithium_polyveck *v0, const dilithium_polyveck *v,
-        uint64_t dilithium_k);
-unsigned int dilithium_polyveck_make_hint(dilithium_polyveck *h,
-                                const dilithium_polyveck *v0,
-                                const dilithium_polyveck *v1,
-                                uint64_t dilithium_k);
-void dilithium_polyveck_use_hint(dilithium_polyveck *w,
-        const dilithium_polyveck *v, const dilithium_polyveck *h,
-        uint64_t dilithium_k);
+void sphincsplus_polyveck_power2round(sphincsplus_polyveck *v1,
+        sphincsplus_polyveck *v0, const sphincsplus_polyveck *v,
+        uint64_t sphincsplus_k);
+void sphincsplus_polyveck_decompose(sphincsplus_polyveck *v1,
+        sphincsplus_polyveck *v0, const sphincsplus_polyveck *v,
+        uint64_t sphincsplus_k);
+unsigned int sphincsplus_polyveck_make_hint(sphincsplus_polyveck *h,
+                                const sphincsplus_polyveck *v0,
+                                const sphincsplus_polyveck *v1,
+                                uint64_t sphincsplus_k);
+void sphincsplus_polyveck_use_hint(sphincsplus_polyveck *w,
+        const sphincsplus_polyveck *v, const sphincsplus_polyveck *h,
+        uint64_t sphincsplus_k);
 
 #endif
